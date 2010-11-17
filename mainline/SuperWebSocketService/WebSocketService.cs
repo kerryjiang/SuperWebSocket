9921d4fbcbfc5d0f9d5ba1a3257165b009e845b8
kerryjiang <none@none.com>
2010-11-17 08:02:49 +0000

    First code committing

---
+++ mainline/SuperWebSocketService/WebSocketService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Configuration;
+using SuperSocket.SocketServiceCore;
+using SuperSocket.SocketServiceCore.Configuration;
+
+namespace SuperWebSocket.Service
+{
+    partial class WebSocketService : ServiceBase
+    {
+        public WebSocketService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            var serverConfig = ConfigurationManager.GetSection("socketServer") as SocketServiceConfig;
+            if (!SocketServerManager.Initialize(serverConfig))
+                return;
+
+            if (!SocketServerManager.Start(serverConfig))
+                SocketServerManager.Stop();
+        }
+
+        protected override void OnStop()
+        {
+            SocketServerManager.Stop();
+            base.OnStop();
+        }
+
+        protected override void OnShutdown()
+        {
+            SocketServerManager.Stop();
+            base.OnShutdown();
+        }
+    }
+}

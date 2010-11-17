40d8543a5642ae831f7bd377bfc855d41523739e
kerryjiang <none@none.com>
2010-11-17 08:02:49 +0000

    First code committing

---
+++ mainline/SuperWebSocketWeb/Global.asax.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Configuration;
+using SuperSocket.SocketServiceCore.Configuration;
+using SuperSocket.SocketServiceCore;
+
+namespace SuperWebSocketWeb
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        void Application_Start(object sender, EventArgs e)
+        {
+            var serverConfig = ConfigurationManager.GetSection("socketServer") as SocketServiceConfig;
+            if (!SocketServerManager.Initialize(serverConfig))
+                return;
+
+            if (!SocketServerManager.Start(serverConfig))
+                SocketServerManager.Stop();
+        }
+
+        void Application_End(object sender, EventArgs e)
+        {
+            SocketServerManager.Stop();
+        }
+
+        void Application_Error(object sender, EventArgs e)
+        {
+            // Code that runs when an unhandled error occurs
+
+        }
+
+        void Session_Start(object sender, EventArgs e)
+        {
+            // Code that runs when a new session is started
+
+        }
+
+        void Session_End(object sender, EventArgs e)
+        {
+            // Code that runs when a session ends. 
+            // Note: The Session_End event is raised only when the sessionstate mode
+            // is set to InProc in the Web.config file. If session mode is set to StateServer 
+            // or SQLServer, the event is not raised.
+
+        }
+
+    }
+}

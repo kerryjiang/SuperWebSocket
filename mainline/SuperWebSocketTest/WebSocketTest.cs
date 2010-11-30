ab11e183b4f1360c12d8680cb35a928a15f04149
kerryjiang <none@none.com>
2010-11-30 06:20:00 +0000

    added test case for message transferring

--- mainline/SuperWebSocketTest/WebSocketTest.cs
+++ mainline/SuperWebSocketTest/WebSocketTest.cs
@@ -51,7 +51,8 @@ namespace SuperWebSocketTest
 
         void m_WebSocketServer_CommandHandler(WebSocketSession session, WebSocketCommandInfo commandInfo)
         {
-
+            Console.WriteLine("Server:" + commandInfo.CommandData);
+            session.SendResponse(commandInfo.CommandData);
         }
 
         [SetUp]
@@ -66,42 +67,115 @@ namespace SuperWebSocketTest
             m_WebSocketServer.Stop();
         }
 
-        [Test]
-        public void HeaderTest()
+        private void Handshake(out Socket socket, out Stream stream)
         {
             var ip = "127.0.0.1";
             var port = 911;
-            using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            var address = new IPEndPoint(IPAddress.Parse(ip), port);
+            socket.Connect(address);
+
+            stream = new NetworkStream(socket);
+
+            var reader = new StreamReader(stream, Encoding.UTF8, false);
+            var writer = new StreamWriter(stream, Encoding.UTF8, 1024 * 10);
+
+            writer.WriteLine("GET /websock HTTP/1.1");
+            writer.WriteLine("Upgrade: WebSocket");
+            writer.WriteLine("Connection: Upgrade");
+            writer.WriteLine("Sec-WebSocket-Key2: 12998 5 Y3 1  .P00");
+            writer.WriteLine("Host: example.com");
+            writer.WriteLine("Sec-WebSocket-Key1: 4 @1  46546xW%0l 1 5");
+            writer.WriteLine("Origin: http://example.com");
+            writer.WriteLine("WebSocket-Protocol: sample");
+            writer.WriteLine("");
+            writer.Write("^n:ds[4U");
+            writer.Flush();
+
+            for (var i = 0; i < 6; i++)
+                Console.WriteLine(reader.ReadLine());
+
+            char[] buffer = new char[20];
+
+            int read = reader.Read(buffer, 0, buffer.Length);
+
+            Assert.AreEqual("8jKS'y:G*Co,Wxa-", new string(buffer.Take(read).ToArray()));
+        }
+
+        [Test]
+        public void HandshakeTest()
+        {
+            Socket socket;
+            Stream stream;
+
+            Handshake(out socket, out stream);
+
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+        }
+
+        [Test, Timeout(5000)]
+        public void MessageTransferTest()
+        {
+            Socket socket;
+            Stream stream;
+
+            Handshake(out socket, out stream);
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < 10; i++)
             {
-                var address = new IPEndPoint(IPAddress.Parse(ip), port);
-                socket.Connect(address);
+                sb.Append(Guid.NewGuid().ToString());
+            }
 
-                var stream = new NetworkStream(socket);
+            string messageSource = sb.ToString();
 
-                using(var reader = new StreamReader(stream, Encoding.UTF8, false))
-                using(var writer = new StreamWriter(stream, Encoding.UTF8, 1024 * 10))
-                {
-                    writer.WriteLine("GET /websock HTTP/1.1");
-                    writer.WriteLine("Upgrade: WebSocket");
-                    writer.WriteLine("Connection: Upgrade");
-                    writer.WriteLine("Sec-WebSocket-Key2: 12998 5 Y3 1  .P00");
-                    writer.WriteLine("Host: example.com");
-                    writer.WriteLine("Sec-WebSocket-Key1: 4 @1  46546xW%0l 1 5");
-                    writer.WriteLine("Origin: http://example.com");
-                    writer.WriteLine("WebSocket-Protocol: sample");
-                    writer.WriteLine("");
-                    writer.Write("^n:ds[4U");
-                    writer.Flush();
+            Random rd = new Random();
 
-                    for (var i = 0; i < 6; i++)
-                        Console.WriteLine(reader.ReadLine());
+            ArraySegmentList<byte> receivedBuffer = new ArraySegmentList<byte>();
 
-                    char[] buffer = new char[20];
+            for (int i = 0; i < 10; i++)
+            {
+                int startPos = rd.Next(0, messageSource.Length - 2);
+                int endPos = rd.Next(startPos + 1, messageSource.Length - 1);
 
-                    int read = reader.Read(buffer, 0, buffer.Length);
+                string currentCommand = messageSource.Substring(startPos, endPos - startPos);
 
-                    Assert.AreEqual("8jKS'y:G*Co,Wxa-", new string(buffer.Take(read).ToArray()));                    
-                }
+                Console.WriteLine("Client:" + currentCommand);
+
+                stream.Write(new byte[] { WebSocketConstant.StartByte }, 0, 1);
+                byte[] data = Encoding.UTF8.GetBytes(currentCommand);
+                stream.Write(data, 0, data.Length);
+                stream.Write(new byte[] { WebSocketConstant.EndByte }, 0, 1);
+                stream.Flush();
+
+                ReceiveMessage(stream, receivedBuffer, data.Length + 2);
+                Assert.AreEqual(data.Length + 2, receivedBuffer.Count);
+                Assert.AreEqual(WebSocketConstant.StartByte, receivedBuffer[0]);
+                Assert.AreEqual(WebSocketConstant.EndByte, receivedBuffer[receivedBuffer.Count - 1]);
+                Assert.AreEqual(currentCommand, Encoding.UTF8.GetString(receivedBuffer.ToArrayData(1, receivedBuffer.Count - 2)));
+                receivedBuffer.ClearSegements();
+            }
+
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+        }
+
+        private void ReceiveMessage(Stream stream, ArraySegmentList<byte> commandBuffer, int predictCount)
+        {
+            byte[] buffer = new byte[1024];
+            int thisRead = 0;
+
+            while ((thisRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                Console.WriteLine("Current read: {0}", thisRead);
+                commandBuffer.AddSegment(new ArraySegment<byte>(buffer.Take(thisRead).ToArray()));
+
+                if (commandBuffer.Count >= predictCount)
+                    break;
             }
         }
     }

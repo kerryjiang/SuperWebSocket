7b1f6a229751130e6e2255d1f703b1963c395542
kerryjiang <none@none.com>
2011-03-07 07:14:43 +0000

    added WebSocketClient project

---
+++ mainline/WebSocketClient/MessageEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperWebSocket.WebSocketClient
+{
+    public class MessageEventArgs : EventArgs
+    {
+        public string Message { get; private set; }
+
+        public MessageEventArgs(string message)
+            : base()
+        {
+            Message = message;
+        }
+    }
+}

dda531e740062becb7492a5930a14ea944039723
kerryjiang <none@none.com>
2011-04-12 14:46:33 +0000

    committed a missing file

---
+++ mainline/SuperWebSocketTest/Command/QUIT.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperWebSocket.SubProtocol;
+using SuperWebSocket;
+using SuperSocket.SocketBase.Command;
+
+namespace SuperWebSocketTest.Command
+{
+    public class QUIT : SubCommandBase
+    {
+        public override void ExecuteCommand(WebSocketSession session, StringCommandInfo commandInfo)
+        {
+            session.Close();
+        }
+    }
+}

ba70f12787deee873488ee49fc4d7424ff6df739
kerryjiang <none@none.com>
2011-04-30 10:00:49 +0000

    add a new sample project CustomSession

---
+++ mainline/Samples/CustomSession/CRMAppServer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperWebSocket.SubProtocol;
+
+namespace SuperWebSocket.Samples.CustomSession
+{
+    public class CRMAppServer : WebSocketServer<CRMSession>
+    {
+        //Because the sample process requests by sub protocol, so passing sub protocol instance to parent class in the line below is required
+        public CRMAppServer()
+            : base(new BasicSubProtocol<CRMSession>(typeof(CRMAppServer).Assembly))            
+        {
+
+        }
+    }
+}

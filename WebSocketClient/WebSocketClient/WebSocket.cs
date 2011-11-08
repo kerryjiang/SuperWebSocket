06ed5eeabed1156e2d8a9a8561f8b44e1d22a189
kerryjiang <none@none.com>
2011-11-08 15:02:24 +0000

    tried to implement new websocket client

--- WebSocketClient/WebSocketClient/WebSocket.cs
+++ WebSocketClient/WebSocketClient/WebSocket.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using SuperSocket.ClientEngine;
 
@@ -8,6 +9,32 @@ namespace SuperWebSocket.WebSocketClient
 {
     public partial class WebSocket : TcpClientSession<WebSocketCommandInfo, WebSocketContext>
     {
+        public WebSocket(string uri)
+            : this(uri, string.Empty, null)
+        {
 
+        }
+
+        public WebSocket(string uri, string subProtocol)
+            : this(uri, subProtocol, null)
+        {
+
+        }
+
+        public WebSocket(string uri, string subProtocol, IEnumerable<Assembly> commandAssemblies)
+            : base(new WebSocketCommandReader(), commandAssemblies)
+        {
+            var targetUri = new Uri(uri);
+
+            if ("wss".Equals(targetUri.Scheme, StringComparison.OrdinalIgnoreCase))
+            {
+                throw new ArgumentException("SuperWebSocket cannot support wss yet.", "uri");
+            }
+
+            if (!"ws".Equals(targetUri.Scheme, StringComparison.OrdinalIgnoreCase))
+            {
+                throw new ArgumentException("Invalid websocket address's schema.", "uri");
+            }
+        }
     }
 }
---
+++ WebSocketClient/WebSocketClient/WebSocketCommandReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.ClientEngine;
+
+namespace SuperWebSocket.WebSocketClient
+{
+    class WebSocketCommandReader : IClientCommandReader<WebSocketCommandInfo>
+    {
+        public WebSocketCommandInfo GetCommandInfo(byte[] readBuffer, int offset, int length, out int left)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

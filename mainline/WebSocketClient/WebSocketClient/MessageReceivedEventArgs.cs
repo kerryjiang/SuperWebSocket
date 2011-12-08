6194c5c2f99b9a4edf048ace0605a2743e90b0d5
kerryjiang <none@none.com>
2011-12-08 15:26:40 +0000

    tried to implement new websocket client

---
+++ mainline/WebSocketClient/WebSocketClient/MessageReceivedEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperWebSocket.WebSocketClient
+{
+    public class MessageReceivedEventArgs : EventArgs
+    {
+        public MessageReceivedEventArgs(string message)
+        {
+            Message = message;
+        }
+
+        public string Message { get; private set; }
+    }
+}
---
+++ mainline/WebSocketClient/WebSocketClient/WebSocketState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperWebSocket.WebSocketClient
+{
+    public enum WebSocketState
+    {
+        Disconnected,
+        Opened
+    }
+}

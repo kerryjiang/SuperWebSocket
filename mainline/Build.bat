a264f0132e4894c6688609b41b45a28dcb1cd480
kerryjiang <none@none.com>
2011-12-27 16:22:15 +0000

    fixed building script issue

--- mainline/Build.bat
+++ mainline/Build.bat
@@ -10,6 +10,10 @@ set msbuild=%fdir%\v4.0.30319\msbuild.exe
 
 %msbuild% SuperWebSocket\SuperWebSocket.csproj /p:Configuration=Release /t:Rebuild /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=..\superwebsocket.snk /p:OutputPath=..\bin\Net40
 
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+
 %msbuild% SuperWebSocket\SuperWebSocket.NET35.csproj  /p:Configuration=Release /t:Rebuild /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=..\superwebsocket.snk /p:OutputPath=..\bin\Net35
+
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
 
 pause

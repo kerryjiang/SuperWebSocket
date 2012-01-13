917b28c13d5400ed94552ec01dc071f093117ce6
kerryjiang <none@none.com>
2012-01-13 01:41:45 +0000

    updated assembly version

--- mainline/GlobalAssemblyInfo.cs
+++ mainline/GlobalAssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("SuperWebSocket")]
 [assembly: AssemblyProduct("SuperWebSocket")]
-[assembly: AssemblyCopyright("Copyright © SuperWebSocket 2011")]
+[assembly: AssemblyCopyright("Copyright © SuperWebSocket 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("0.0.8.0")]
-[assembly: AssemblyFileVersion("0.0.8.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]

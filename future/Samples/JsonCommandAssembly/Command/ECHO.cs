1e8b16a3a01e2863fa8d283c5be2dbcaca482a3f
kerryjiang <none@none.com>
2012-02-16 15:10:51 +0000

    create future branch which bases on SuperSocket 1.5

---
+++ future/Samples/JsonCommandAssembly/Command/ECHO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperWebSocket.SubProtocol;
+using SuperWebSocket.Samples.JsonCommandAssembly.JsonObject;
+
+namespace SuperWebSocket.Samples.JsonCommandAssembly.Command
+{
+    public class ECHO : JsonSubCommand<ChatMessage>
+    {
+        protected override void ExecuteJsonCommand(WebSocketSession session, string token, ChatMessage commandInfo)
+        {
+            //Send the received message back to client
+            SendJsonResponse(session, token, commandInfo);
+        }
+    }
+}
---
+++ future/Samples/JsonCommandAssembly/JsonObject/ChatMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperWebSocket.Samples.JsonCommandAssembly.JsonObject
+{
+    public class ChatMessage
+    {
+        public string Sender { get; set; }
+        public string Receiver { get; set; }
+        public string Content { get; set; }
+    }
+}

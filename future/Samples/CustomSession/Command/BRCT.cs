b8e674d8657d2dfa7e070cf4e014ba11b54c25fc
kerryjiang <none@none.com>
2012-02-16 15:10:51 +0000

    create future branch which bases on SuperSocket 1.5

---
+++ future/Samples/CustomSession/Command/BRCT.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperWebSocket.Samples.CustomSession.JsonObject;
+using SuperWebSocket.SubProtocol;
+
+namespace SuperWebSocket.Samples.CustomSession.Command
+{
+    /// <summary>
+    /// Broadcast messages to sessions below to specific company
+    /// </summary>
+    public class BRCT : JsonSubCommand<CRMSession, BroadcastMessage>
+    {
+        protected override void ExecuteJsonCommand(CRMSession session, string token, BroadcastMessage commandInfo)
+        {
+            if (commandInfo.CompanyId <= 0)
+                return;
+
+            string message = GetJsonResponse("MSG", token, commandInfo);
+
+            foreach(var s in session.AppServer.GetSessions(s => s.CompanyId == commandInfo.CompanyId))
+            {
+                s.SendResponseAsync(message);
+            }
+        }
+    }
+}
---
+++ future/Samples/CustomSession/JsonObject/BroadcastMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperWebSocket.Samples.CustomSession.JsonObject
+{
+    public class BroadcastMessage
+    {
+        public int CompanyId { get; set; }
+
+        public string Content { get; set; }
+    }
+}

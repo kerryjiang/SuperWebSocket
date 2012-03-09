fee342b0130c8306ce7394fa2994855d093ee2c8
kerryjiang <none@none.com>
2012-03-09 05:03:21 +0000

    mainline/future: fixed a bug about sending closing handshake

--- future/SuperWebSocket/Protocol/DraftHybi10Processor.cs
+++ future/SuperWebSocket/Protocol/DraftHybi10Processor.cs
@@ -98,7 +98,7 @@ namespace SuperWebSocket.Protocol
 
         public override void SendCloseHandshake(IWebSocketSession session, int statusCode, string closeReason)
         {
-            byte[] playloadData = new byte[(string.IsNullOrEmpty(closeReason) ? 2 : Encoding.UTF8.GetMaxByteCount(closeReason.Length)) + 2];
+            byte[] playloadData = new byte[(string.IsNullOrEmpty(closeReason) ? 0 : Encoding.UTF8.GetMaxByteCount(closeReason.Length)) + 2];
 
             int highByte = statusCode / 256;
             int lowByte = statusCode % 256;

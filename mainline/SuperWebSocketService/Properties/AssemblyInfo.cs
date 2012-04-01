1c02f93207646d3389c262bdfa8bce56bf749089
kerryjiang <none@none.com>
2012-04-01 15:23:55 +0000

    mainline/future: improved sub protocol implementation

--- mainline/SuperWebSocketService/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("SuperWebSocketService")]
-[assembly: AssemblyDescription("SuperWebSocketService")]
-[assembly: AssemblyConfiguration("")]
-[assembly: ComVisible(false)]
-[assembly: Guid("b9c789cd-bead-4ab8-9d5b-bf4034391a19")]

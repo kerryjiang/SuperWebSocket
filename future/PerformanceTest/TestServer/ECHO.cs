fadc77f7d1674c51caa08740fd36cb12707a2755
kerryjiang <none@none.com>
2012-04-04 07:51:13 +0000

    mainline/future: added performance test samples

---
+++ future/PerformanceTest/TestServer/ECHO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperWebSocket.SubProtocol;
+
+namespace SuperWebSocket.PerformanceTest.TestServer
+{
+    public class ECHO : JsonSubCommand<TestSession, string>
+    {
+        protected override void ExecuteJsonCommand(TestSession session, string commandInfo)
+        {
+            SendJsonResponse(session, commandInfo);
+        }
+    }
+}

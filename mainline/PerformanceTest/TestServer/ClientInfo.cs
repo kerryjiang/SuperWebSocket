67bf0ed0d043d96c8059a2a0966b8f878b0ee3db
kerryjiang <none@none.com>
2012-07-31 07:56:08 +0000

    mainline: added more code in PerformanceTest projects

---
+++ mainline/PerformanceTest/TestServer/ClientInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperWebSocket.PerformanceTest.TestServer
+{
+    public class ClientInfo
+    {
+        public int ID { get; set; }
+
+        public int LocationX { get; set; }
+
+        public int LocationY { get; set; }
+
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+
+        public string TargetID { get; set; }
+    }
+}
--- mainline/PerformanceTest/TestServer/TestSession.cs
+++ mainline/PerformanceTest/TestServer/TestSession.cs
@@ -5,8 +5,19 @@ using System.Text;
 
 namespace SuperWebSocket.PerformanceTest.TestServer
 {
-    public class TestSession : WebSocketSession<TestSession>
+    public class TestSession : JsonWebSocketSession<TestSession>
     {
+        protected override void OnHandShaked()
+        {
+            base.OnHandShaked();
 
+            if (AppServer.SessionCount <= 1)
+                return;
+
+            foreach (var x in AppServer.GetSessions(s => !s.SessionID.Equals(this.SessionID)))
+            {
+                x.SendJsonResponse("NEW", this.SessionID);
+            }
+        }
     }
 }

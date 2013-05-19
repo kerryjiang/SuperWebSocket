8bc9901f9bde367c86e932733e4c5e7366a11da9
Kerry Jiang <kerryjiang@outlook.com>
2013-05-19 18:36:30 +0800

    updated PerformanceTest

---
+++ PerformanceTest/PushTestClient/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WebSocket4Net;
+using System.Threading;
+
+namespace PushTestClient
+{
+    class Program
+    {
+        private static WebSocket[] m_WebSockets;
+
+        static void Main(string[] args)
+        {
+            m_WebSockets = new WebSocket[1000];
+
+            var autoEventReset = new AutoResetEvent(false);
+
+            for(var i = 0; i < m_WebSockets.Length; i++)
+            {
+                var websocket = new WebSocket("ws://localhost:2011/");
+                websocket.Opened += (s, e) =>
+                    {
+                        autoEventReset.Set();
+                    };
+                websocket.Error +=  (s, e) =>
+                    {
+                        Console.WriteLine(e.Exception.Message);
+                        autoEventReset.Set();
+                    };
+
+                websocket.MessageReceived += new EventHandler<MessageReceivedEventArgs>(websocket_MessageReceived);
+                websocket.Open();
+                autoEventReset.WaitOne();
+
+                m_WebSockets[i] = websocket;
+
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("All connected");
+            Console.ReadLine();
+        }
+
+        static void websocket_MessageReceived(object sender, MessageReceivedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ PerformanceTest/PushTestServer/PushTestAppServer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace SuperWebSocket.PerformanceTest.PushTestServer
+{
+    public class PushTestAppServer : WebSocketServer
+    {
+        private Timer m_Timer;
+
+        protected override void OnStartup()
+        {
+            m_Timer = new Timer(TimerCallback);
+            m_Timer.Change(100, 100);
+
+            base.OnStartup();
+        }
+
+        void TimerCallback(object state)
+        {
+            m_Timer.Change(Timeout.Infinite, Timeout.Infinite);
+
+            try
+            {
+                foreach (var s in this.GetAllSessions())
+                {
+                    s.Send("{\"message\": \"_ping\"}");
+                }
+            }
+            catch (Exception e)
+            {
+                Logger.Error(e);
+            }
+
+            m_Timer.Change(100, 100);
+        }
+
+        protected override void OnStopped()
+        {
+            m_Timer.Change(Timeout.Infinite, Timeout.Infinite);
+            m_Timer.Dispose();
+            m_Timer = null;
+
+            base.OnStopped();
+        }
+    }
+}
